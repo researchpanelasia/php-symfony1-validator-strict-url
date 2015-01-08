d1cb39e98b7cbfb6a31961be59c88e7219d22511
yowcowvg <yoko_ohyama@voyagegroup.com>
2015-01-08 15:57:27 +0900

    comment

--- lib/sfValidatorStrictUrl.php
+++ lib/sfValidatorStrictUrl.php
@@ -6,6 +6,7 @@ class sfValidatorStrictUrl extends \sfValidatorRegex
     {
         parent::configure($options, $messages);
 
+        # Taken from http://www.din.or.jp/~ohzaki/perl.htm#httpURL
         $this->setOption('pattern', '/\As?https?:\/\/[-_.!~*\'()a-zA-Z0-9;\/?:\@&=+\$,%#]+\z/i');
     }
 }
